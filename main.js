2e100e083c52a8b290f8617835d87f59a89cd6fa
stabti <156535043+stabti@users.noreply.github.com>
2025-05-19 18:50:53 +0200

    Update main.js
    remove imports

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
 // Load App component
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(React.createElement(App));
